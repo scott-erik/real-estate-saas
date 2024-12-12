b4bc7dd616d2760e1c29ae5053c3f194ee0091e8
Erik <45295020+scott-erik@users.noreply.github.com>
2024-12-12 17:25:21 -0500

    updated visitor form controller

--- backend/controllers/openHouseController.js
+++ backend/controllers/openHouseController.js
@@ -2,10 +2,24 @@ const OpenHouse = require('../models/OpenHouse');
 
 exports.createOpenHouse = async (req, res) => {
   const { address, description } = req.body;
-  const formLink = `https://yourapp.com/form/${Date.now()}`; // Temporary link generator
-  const newOpenHouse = new OpenHouse({ agentId: req.user.id, address, description, formLink });
+
   try {
+    // Create a new Open House document
+    const newOpenHouse = new OpenHouse({
+      agentId: req.user.id,
+      address,
+      description,
+    });
+
+    // Save the new Open House to generate the `_id`
     await newOpenHouse.save();
+
+    // Update the formLink with the Open House's _id
+    newOpenHouse.formLink = `https://yourapp.com/form/${newOpenHouse._id}`;
+
+    // Save the updated Open House with the formLink
+    await newOpenHouse.save();
+
     res.status(201).json(newOpenHouse);
   } catch (error) {
     res.status(500).json({ error: error.message });
